c702df2a07cb73c2b5ccaf17079781dda184d211
iwouldneverto <glebsmile3303@gmail.com>
2024-10-18 17:50:36 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import React from 'react';
 
+const App: React.FC = () => {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+            <div className="bg-blue-500">Block 1</div>
+            <div className="bg-red-500">Block 2</div>
+            <div className="bg-green-500">Block 3</div>
+            <div className="bg-yellow-500">Block 4</div>
+        </div>
 
-function App() {
-  return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1>MVP</h1>
-  
-    </div>
-  );
-}
+    );
+};
 
 export default App;
