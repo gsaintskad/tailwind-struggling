a03afbc5ef95eb0f199e5bdcf3c5f987f1c554ce
iwouldneverto <glebsmile3303@gmail.com>
2024-10-18 18:26:01 +0200

    there was added few grid usage examples with media queries

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const App: React.FC = () => {
-    return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-            <div className="bg-blue-500">Block 1</div>
-            <div className="bg-red-500">Block 2</div>
-            <div className="bg-green-500">Block 3</div>
-            <div className="bg-yellow-500">Block 4</div>
-        </div>
-
-    );
+  return (
+    <>
+      <div className="m-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:bg-red-500 xl:grid-cols-4 gap-4">
+        <div className="bg-red-500 p-4">Item 2</div>
+        <div className="bg-indigo-500 p-4">Item 5</div>
+        <div className="bg-blue-500 p-4">Item 1</div>
+        <div className="bg-green-500 p-4">Item 3</div>
+        <div className="bg-yellow-500 p-4">Item 4</div>
+        <div className="bg-purple-500 p-4">Item 6</div>
+      </div>
+      <div className="m-10 grid grid-cols-3 grid-rows-2 gap-4">
+        <div className="col-span-2 bg-blue-500">Spans 2 columns</div>
+        <div className="bg-red-500">Normal 1 column span</div>
+        <div className="col-span-3 bg-green-500">Spans all 3 columns</div>
+        <div className="row-span-2 bg-yellow-500">Spans 2 rows</div>
+      </div>
+    </>
+  );
 };
 
 export default App;
